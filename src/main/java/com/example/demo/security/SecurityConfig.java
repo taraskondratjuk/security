88c98fd5ab5443d5c0b312e5f88deb736295daa3
TarasKondratiuk <taraskondratjuk@.gmail.com>
2021-08-29 14:38:05 +0300

    vnvbnvbn

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -12,6 +12,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "index")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
